0ce8badd30a4f0ea0ea8e3b7669e0203bcc9757c
Victor Getsov <viktorgetsov@gmail.com>
2023-07-26 23:23:49 +0300

    add prettier, eslint-config-prettier, typescript-eslint

---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    printWidth: 120, // how many symbols on a single line before prettier tries to break it
+    semi: false, // optional semi-colons off
+    singleQuote: true,
+}
